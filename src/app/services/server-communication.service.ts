8c01b59a11539453b04c25df62fb7428532266b6
Chris Jeamme <chrisjahim@free.fr>
2021-01-22 03:56:52 +0100

    feat: add test http

--- src/app/services/server-communication.service.ts
+++ src/app/services/server-communication.service.ts
@@ -1,16 +1,34 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ServerCommunicationService {
+export class ServerCommunicationService
+{
 
   constructor(
     private http: HttpClient
   ) { }
 
-
+  testOk()
+  {
+    this.http.get('https://my-json-server.typicode.com/typicode/demo/posts').subscribe(
+      success =>
+      {
+        console.log(success);
+      }
+    )
+  }
+  testFail()
+  {
+    this.http.get('https://my-json-server.typicodes.com/typicode/demo/posts').subscribe(
+      success =>
+      {
+        console.log(success);
+      }
+    )
+  }
 }
